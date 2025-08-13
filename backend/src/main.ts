613f46dede9379d06f3d3f16ff45f8e97cb38025
kris <ng.phihoc123@gmail.com>
2025-08-14 01:02:48 +0700

    chore: config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,42 +1,37 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '../src/app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { TransformInterceptor } from './core/interceptors';
-import { HttpExceptionFilter } from './core/filters';
-import { INestApplication, ValidationPipe } from '@nestjs/common';
-let cachedApp: INestApplication;
-async function bootstrap(): Promise<INestApplication> {
-  if (cachedApp) {
-    return cachedApp;
+import { ValidationPipe } from '@nestjs/common';
+import { TransformInterceptor } from '../src/core/interceptors';
+import { HttpExceptionFilter } from '../src/core/filters';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+
+let cachedApp: any;
+
+async function bootstrap() {
+  if (!cachedApp) {
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+    app.enableCors({ origin: true, credentials: true });
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
+    app.useGlobalInterceptors(new TransformInterceptor());
+    app.useGlobalFilters(new HttpExceptionFilter());
+
+    const config = new DocumentBuilder()
+      .setTitle('Mindflip API')
+      .setDescription('Tài liệu API')
+      .setVersion('1.0')
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, document);
+
+    await app.init();
+    cachedApp = app;
   }
-
-  const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: true, 
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
-  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
-  app.useGlobalInterceptors(new TransformInterceptor());
-  app.useGlobalFilters(new HttpExceptionFilter());
-
-  const config = new DocumentBuilder()
-    .setTitle('Mindflip API')
-    .setDescription('Tài liệu API cho ứng dụng học từ vựng')
-    .setVersion('1.0')
-    .addTag('API')
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document);
-  await app.init();
-
-  cachedApp = app;
-  return app;
+  return cachedApp;
 }
 
-export default async (req: any, res: any) => {
+export default async (req: VercelRequest, res: VercelResponse) => {
   const app = await bootstrap();
   const server = app.getHttpAdapter().getInstance();
   server(req, res);
