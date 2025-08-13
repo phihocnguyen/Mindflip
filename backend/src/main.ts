a39f1c4d5642bad7b5d7f898ebc4b97d62a1645a
kris <ng.phihoc123@gmail.com>
2025-08-14 00:13:22 +0700

    deploy backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,26 +3,24 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { TransformInterceptor } from './core/interceptors';
 import { HttpExceptionFilter } from './core/filters';
-import { ValidationPipe } from '@nestjs/common';
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+let cachedApp: INestApplication;
+async function bootstrap(): Promise<INestApplication> {
+  if (cachedApp) {
+    return cachedApp;
+  }
 
-  // Cấu hình CORS và các middleware khác nếu cần
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: true, 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
-  app.setGlobalPrefix('api'); // Thêm tiền tố /api cho tất cả các route
-  app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  // Áp dụng interceptor transform toàn cục
+  app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
-
-  // Áp dụng filter exception toàn cục
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  // --- Bắt đầu cấu hình Swagger ---
   const config = new DocumentBuilder()
     .setTitle('Mindflip API')
     .setDescription('Tài liệu API cho ứng dụng học từ vựng')
@@ -31,9 +29,15 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('docs', app, document); // UI sẽ có tại /docs
-  // --- Kết thúc cấu hình Swagger ---
+  SwaggerModule.setup('docs', app, document);
+  await app.init();
 
-  await app.listen(3333);
+  cachedApp = app;
+  return app;
 }
-bootstrap();
+
+export default async (req: any, res: any) => {
+  const app = await bootstrap();
+  const server = app.getHttpAdapter().getInstance();
+  server(req, res);
+};
